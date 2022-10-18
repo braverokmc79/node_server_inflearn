d965e48a878dc219605e8c45cec2abec07532eb1
braverokmc79 <braverokmc79@gmail.com>
2022-10-18 10:48:33 +0900

    3) URL Routing 처리

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ app.listen(3000, function () {
     console.log("start! express server on port 3000");
 });
 
-console.log("end of server code....");
+
+app.get("/", function (req, res) {
+    //res.send("<h1>h1 friend</h1>");
+    res.sendFile(__dirname + "/public/main.html");
+})
