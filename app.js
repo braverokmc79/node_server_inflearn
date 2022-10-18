3e83d3a6f95b4fdb652f36f4421b02120414c9f5
braverokmc79 <braverokmc79@gmail.com>
2022-10-18 10:59:22 +0900

    4) static 디렉토리 설정

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ app.listen(3000, function () {
     console.log("start! express server on port 3000");
 });
 
+//static 파일 등록 처리
+app.use(express.static("public"));
+
 
 app.get("/", function (req, res) {
+    console.log("test");
     //res.send("<h1>h1 friend</h1>");
+    res.sendFile(__dirname + "/public/main.html");
+})
+
+app.get("/main", function (req, res) {
     res.sendFile(__dirname + "/public/main.html");
 })
